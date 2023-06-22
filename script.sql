7a8703fcec4c5bbb3495dd8aded807c0d60462de
Agustin Barreto <67488070+agustin200208@users.noreply.github.com>
2023-06-22 13:23:08 -0300

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,70 @@
+create [pruebademo]
+USE [pruebademo]
+GO
+/****** Object:  Table [dbo].[clientes]    Script Date: 21/6/2023 14:45:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[clientes](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Cliente] [varchar](255) NOT NULL,
+	[Telefono] [varchar](255) NULL,
+	[Correo] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[productos]    Script Date: 21/6/2023 14:45:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[productos](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](255) NOT NULL,
+	[Precio] [float] NULL,
+	[Categoria] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ventas]    Script Date: 21/6/2023 14:45:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ventas](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IDCliente] [int] NOT NULL,
+	[Fecha] [datetime] NULL,
+	[Total] [float] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ventasitems]    Script Date: 21/6/2023 14:45:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ventasitems](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IDVenta] [int] NOT NULL,
+	[IDProducto] [int] NOT NULL,
+	[PrecioUnitario] [float] NULL,
+	[Cantidad] [float] NULL,
+	[PrecioTotal] [float] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
